749a09a38c37aa2bbc22ef5d1ff8190220730655
Gena Jey <thipi1213@live.com>
2025-01-11 22:42:36 -0500

    Made changes to index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// const { google } = require('googleapis');
-
 import { google } from "googleapis";
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+
 
 const port = process.env.PORT || 8000;
 
