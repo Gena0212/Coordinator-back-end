170b0c1c6cd585717ca22f5008074d2730fcd6ea
Gena Jey <thipi1213@live.com>
2025-01-14 14:35:49 -0500

    Add groups migrations file

---
+++ migrations/20250114193313_create_group_tables.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema.createTable("groups", function (table) {
+        table.increments("id").primary();
+
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema.dropTable("groups");
+};
