e4c80387e2662713694101271604b73c6c82e6d0
Gena Jey <thipi1213@live.com>
2025-01-17 16:15:23 -0500

    Remove unnecessary code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import cookieParser from 'cookie-parser';
 
 import requestRoutes from "./routes/request.js";
 import oauthRoutes from "./routes/oauth.js";
@@ -14,7 +13,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser());
 
 app.use('/request', requestRoutes);
 app.use('/oauth', oauthRoutes)
